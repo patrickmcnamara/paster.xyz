38bd95cb560a92ec629c43fe70f52b0c25af8dd5
Patrick McNamara <hello@patrickmcnamara.xyz>
2019-01-22 12:33:51 +0000

    Remove unnecessary 404 handler

--- handler.go
+++ handler.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"html/template"
-	"io/ioutil"
 	"net/http"
 )
-
-func notFoundHandler(w http.ResponseWriter) {
-	w.WriteHeader(http.StatusNotFound)
-	data, _ := ioutil.ReadFile("static/not-found.html")
-	w.Write(data)
-}
 
 func errorHandler(w http.ResponseWriter, title, desc string, statusCode int) {
 	w.WriteHeader(statusCode)
