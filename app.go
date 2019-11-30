298630c33e51112ce3fae438b603220b1b3b08bd
Patrick McNamara <hello@patrickmcnamara.xyz>
2019-11-30 20:46:30 +0000

    Remove users and list and expiry options

--- app.go
+++ app.go
@@ -22,48 +22,33 @@ type app struct {
 	DB *sql.DB
 }
 
-func (a *app) getPaste(pasteID id) (p paste, err error) {
-	r := a.DB.QueryRow("SELECT Value FROM paste WHERE ID = ? AND (Expiry IS NULL OR Expiry > NOW())", pasteID)
+func (a *app) setPaste(p paste) error {
+	_, err := a.DB.Exec("INSERT INTO paste (ID, Value, Time) VALUES (?, ?, ?)", p.ID, p.Value, p.Time)
+	return err
+}
+
+func (a *app) getPaste(id []byte) (p paste, err error) {
+	r := a.DB.QueryRow("SELECT Value FROM paste WHERE ID = ?", id)
 	err = r.Scan(&p.Value)
 	return
 }
 
-func (a *app) setPaste(p paste) error {
-	_, err := a.DB.Exec("INSERT INTO paste (ID, Value, Time, Expiry, User, List) VALUES (?, ?, ?, ?, ?, ?)", p.ID, p.Value, p.Time, p.Expiry, p.User, p.List)
-	return err
-}
-
-func (a *app) getHistoryPastes(user id) (ps []paste, err error) {
-	rows, err := a.DB.Query("SELECT ID, Time, Expiry FROM paste WHERE User = ? AND (Expiry IS NULL OR Expiry > NOW()) ORDER by Time DESC LIMIT ?", user, pasteListLength)
-	defer rows.Close()
-	if err != nil {
-		return
-	}
-	for rows.Next() {
-		var p paste
-		p.User = user
-		rows.Scan(&p.ID, &p.Time, &p.Expiry)
-		ps = append(ps, p)
-	}
-	return
-}
-
 func (a *app) getRecentPastes() (ps []paste, err error) {
-	rows, err := a.DB.Query("SELECT ID, Time, Expiry FROM paste WHERE List AND (Expiry IS NULL OR Expiry > NOW()) ORDER BY Time DESC LIMIT ?", pasteListLength)
+	rows, err := a.DB.Query("SELECT ID, Time FROM paste ORDER BY Time DESC LIMIT ?", pasteListLength)
 	defer rows.Close()
 	if err != nil {
 		return
 	}
 	for rows.Next() {
 		var p paste
-		rows.Scan(&p.ID, &p.Time, &p.Expiry)
+		rows.Scan(&p.ID, &p.Time)
 		ps = append(ps, p)
 	}
 	return
 }
 
 func (a *app) getAllPastes() (ps []paste, err error) {
-	rows, err := a.DB.Query("SELECT ID, Time, Value FROM paste WHERE List AND (Expiry IS NULL OR Expiry > NOW()) ORDER BY Time DESC")
+	rows, err := a.DB.Query("SELECT ID, Time, Value FROM paste ORDER BY Time DESC")
 	defer rows.Close()
 	if err != nil {
 		return
@@ -76,9 +61,9 @@ func (a *app) getAllPastes() (ps []paste, err error) {
 	return
 }
 
-func (a *app) getLatestPasteID() (i id, err error) {
-	r := a.DB.QueryRow("SELECT ID FROM paste WHERE List AND (Expiry IS NULL OR Expiry > NOW()) ORDER BY Time DESC LIMIT 1")
-	err = r.Scan(&i)
+func (a *app) getLatestPasteID() (id []byte, err error) {
+	r := a.DB.QueryRow("SELECT ID FROM paste ORDER BY Time DESC LIMIT 1")
+	err = r.Scan(&id)
 	return
 }
 
@@ -92,13 +77,8 @@ func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		switch path {
 		// serve homepage
 		case "/":
-			now := time.Now().UTC()
 			t, _ := template.ParseFiles("template/page.tmpl", "template/index.tmpl")
-			t.ExecuteTemplate(w, "index-page", map[string]interface{}{
-				"Title":   "index",
-				"MinTime": now,
-				"MaxTime": now.AddDate(5, 0, 0),
-			})
+			t.ExecuteTemplate(w, "index-page", nil)
 			log.Printf("%s - %s - homepage", method, path)
 
 		// don't serve favicon and don't log
@@ -108,6 +88,7 @@ func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		// status page
 		case "/status":
 			fmt.Fprintln(w, "UP")
+			log.Printf("%s - %s - status check", method, path)
 
 		// serve latest paste
 		case "/latest":
@@ -118,33 +99,12 @@ func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				log.Printf("%s - %s - %s", method, path, errS)
 				return
 			}
-			http.Redirect(w, r, "/"+id.String(), http.StatusSeeOther)
-
-		// list history pastes
-		case "/history":
-			t, _ := template.ParseFiles("template/page.tmpl", "template/list.tmpl")
-			if c, err := r.Cookie("user"); err == nil {
-				user, _ := base64.RawURLEncoding.DecodeString(c.Value)
-				pastes, err := a.getHistoryPastes(user)
-				if err != nil {
-					errS := "could not list history"
-					errorHandler(w, errS, err.Error(), http.StatusInternalServerError)
-					log.Printf("%s - %s - %s", method, path, errS)
-					return
-				}
-				t.ExecuteTemplate(w, "list-page", map[string]interface{}{
-					"Title":  title,
-					"Pastes": pastes,
-				})
-				log.Printf("%s - %s - user cookie found, listing history", method, path)
-			} else {
-				t.ExecuteTemplate(w, "list-page", map[string]interface{}{"Title": title})
-				log.Printf("%s - %s - user cookie not found, no history", method, path)
-			}
+			http.Redirect(w, r, "/"+base64.RawURLEncoding.EncodeToString(id), http.StatusSeeOther)
+			log.Printf("%s - %s - redirecting to %s", method, path, "/"+base64.RawURLEncoding.EncodeToString(id))
 
 		// list recent pastes
 		case "/recent":
-			t, _ := template.ParseFiles("template/page.tmpl", "template/list.tmpl")
+			t, _ := template.ParseFiles("template/page.tmpl", "template/recent.tmpl")
 			pastes, err := a.getRecentPastes()
 			if err != nil {
 				errS := "could not list recent pastes"
@@ -152,10 +112,12 @@ func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				log.Printf("%s - %s - %s", method, path, errS)
 				return
 			}
-			t.ExecuteTemplate(w, "list-page", map[string]interface{}{
-				"Title":  title,
-				"Pastes": pastes,
-			})
+			recent := make([][2]string, len(pastes))
+			for i, p := range pastes {
+				recent[i][0] = base64.RawURLEncoding.EncodeToString(p.ID)
+				recent[i][1] = p.Time.Format("2006-01-02 15:04:05")
+			}
+			t.ExecuteTemplate(w, "recent-page", recent)
 			log.Printf("%s - %s - listing recent pastes", method, path)
 
 		// other stuff
@@ -173,13 +135,14 @@ func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			defer tgz.Close()
 			for _, paste := range ps {
 				tgz.WriteHeader(&tar.Header{
-					Name:    base64.RawURLEncoding.EncodeToString(paste.ID),
+					Name:    base64.RawURLEncoding.EncodeToString(paste.ID[:]),
 					Size:    int64(len(paste.Value)),
 					Mode:    0666,
 					ModTime: paste.Time,
 				})
 				tgz.Write([]byte(paste.Value))
 			}
+			log.Printf("%s - %s - generated tar.gz archive", method, path)
 
 		// get paste if it exists, else return a 404
 		default:
@@ -196,86 +159,36 @@ func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	case "POST":
 		// parse values
-		err := r.ParseForm()
-		if err != nil {
+		if err := r.ParseForm(); err != nil {
 			errorHandler(w, "could not set paste", err.Error(), http.StatusInternalServerError)
 			log.Printf("%s - %s - could not submit paste - %v", method, path, err)
 			return
 		}
 
 		// generate paste ID
-		pasteID := generateID()
+		id := generateID()
 
-		// get Value and validate
+		// get value and validate
 		value := r.FormValue("Value")
 		if len(value) == 0 {
 			errorHandler(w, "value too short", "value must not be empty", http.StatusBadRequest)
 			log.Printf("%s - %s - could not submit paste, too short", method, path)
 			return
-		} else if len([]byte(value)) >= pasteLength {
+		} else if len(value) >= pasteLength {
 			errorHandler(w, "value too long", "value must be less than 5 mebibytes", http.StatusRequestEntityTooLarge)
 			log.Printf("%s - %s - could not submit paste, too long", method, path)
 			return
 		}
 
-		// get List
-		list := r.FormValue("List") == "list"
-
-		// get Expiry and validate
-		expiryValue := r.FormValue("Expiry")
-		expiryTime, err := time.Parse(time.RFC3339[:16], expiryValue)
-		var expiry nullTime
-		if expiryValue == "" {
-			expiry = nullTime{expiryTime, false}
-		} else if err != nil {
-			errorHandler(w, "invalid expiry", "expiry must be after the current time", http.StatusBadRequest)
-			log.Printf("%s - %s - could not submit paste, invalid expiry", method, path)
-			return
-		} else if !expiryTime.After(time.Now()) {
-			errorHandler(w, "expiry before current time", "expiry must be after the current time", http.StatusBadRequest)
-			log.Printf("%s - %s - could not submit paste, expiry before current time", method, path)
-			return
-		} else {
-			expiry = nullTime{expiryTime, true}
-		}
-
-		// assign to User, if it exists, else generate User
-		var user id
-		if c, err := r.Cookie("user"); err == nil {
-			user, _ = base64.RawURLEncoding.DecodeString(c.Value)
-			http.SetCookie(w, &http.Cookie{
-				Name:    "user",
-				Expires: time.Now().Add(time.Hour * 24 * 365),
-				Value:   c.Value,
-			})
-			log.Printf("%s - %s - user cookie found, refreshing cookie", method, path)
-		} else {
-			user = generateID()
-			http.SetCookie(w, &http.Cookie{
-				Name:    "user",
-				Expires: time.Now().Add(time.Hour * 24 * 365),
-				Value:   base64.RawURLEncoding.EncodeToString(user),
-			})
-			log.Printf("%s - %s - user cookie not found, setting cookie", method, path)
-		}
-
 		// create and set paste
-		err = a.setPaste(paste{
-			ID:     pasteID,
-			Value:  value,
-			Time:   time.Now().UTC(),
-			Expiry: expiry,
-			User:   user,
-			List:   list,
-		})
-		if err != nil {
+		if err := a.setPaste(paste{ID: id, Value: value, Time: time.Now().UTC()}); err != nil {
 			errorHandler(w, "could not set paste", err.Error(), http.StatusInternalServerError)
 			log.Printf("%s - %s - could not submit paste - %v", method, path, err)
 			return
 		}
 
 		// redirect to new paste
-		http.Redirect(w, r, "/"+pasteID.String(), http.StatusSeeOther)
-		log.Printf("%s - %s - paste submitted, redirecting to %s", method, path, "/"+pasteID.String())
+		http.Redirect(w, r, "/"+base64.RawURLEncoding.EncodeToString(id[:]), http.StatusSeeOther)
+		log.Printf("%s - %s - paste submitted, redirecting to %s", method, path, "/"+base64.RawURLEncoding.EncodeToString(id[:]))
 	}
 }
--- paste.go
+++ paste.go
@@ -2,45 +2,17 @@ package main
 
 import (
 	"crypto/rand"
-	"database/sql/driver"
-	"encoding/base64"
 	"time"
 )
 
-type id []byte
-
-type nullTime struct {
-	Time  time.Time
-	Valid bool
-}
-
-func (n *nullTime) Scan(value interface{}) error {
-	n.Time, n.Valid = value.(time.Time)
-	return nil
-}
-
-func (n nullTime) Value() (driver.Value, error) {
-	if !n.Valid {
-		return nil, nil
-	}
-	return n.Time, nil
-}
-
-func (ID id) String() string {
-	return base64.RawURLEncoding.EncodeToString(ID)
-}
-
-func generateID() id {
-	ID := make([]byte, 8)
-	rand.Read(ID)
-	return ID
-}
-
 type paste struct {
-	ID     id
-	Value  string
-	Time   time.Time
-	Expiry nullTime
-	User   id
-	List   bool
+	ID    []byte
+	Value string
+	Time  time.Time
+}
+
+func generateID() (id []byte) {
+	id = make([]byte, 8)
+	rand.Read(id)
+	return id
 }
