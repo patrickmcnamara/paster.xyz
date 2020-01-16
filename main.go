d33872a71f77a733935a6f732b01297fd607e9da
Patrick McNamara <hello@patrickmcnamara.xyz>
2020-01-16 13:14:44 +0000

    More precise initial log messages

--- main.go
+++ main.go
@@ -10,29 +10,32 @@ import (
 
 func main() {
 	// load database configuration file
+	log.Println("Reading config file...")
 	cfg, err := loadDbConfig("db-config.json")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
+	log.Println("Config file OK.")
 
 	// open database and test connection
 	log.Println("Setting up database...")
 	dataSourceName := cfg.getDataSourceName()
 	db, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 	defer db.Close()
-	log.Println("Testing database connection...")
+	log.Println("Pinging database...")
 	err = db.Ping()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
+	log.Println("Database OK.")
 
 	// start server
 	log.Println("Starting server...")
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 	http.Handle("/", &app{db})
 	log.Print("Serving HTTP...")
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatalln(http.ListenAndServe(":80", nil))
 }
